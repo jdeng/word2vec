93dc19fbf2c96c0cc4813b61f499c2fcf699d2f1
Wladimir Sidorenko <wlsidorenko@gmail.com>
2015-11-06 17:33:26 +0100

    cosmetic changes to get rif of compiler warnings

--- main.cc
+++ main.cc
@@ -5,140 +5,140 @@
 #include <iostream>
 #include <initializer_list>
 
-int accuracy(Word2Vec<std::string>& model, std::string questions, int restrict_vocab = 30000) {
-	std::ifstream in(questions);
-	std::string line;
-	auto lower = [](std::string& data) { std::transform(data.begin(), data.end(), data.begin(), ::tolower);};
-	size_t count = 0, correct = 0, ignore = 0, almost_correct = 0;
-	const int topn = 10;
-	while (std::getline(in, line)) {
-		if (line[0] == ':') {
-			printf("%s\n", line.c_str());
-			continue;
-		}
+int accuracy(Word2Vec<std::string>& model, std::string questions) {
+  std::ifstream in(questions);
+  std::string line;
+  auto lower = [](std::string& data) { std::transform(data.begin(), data.end(), data.begin(), ::tolower);};
+  size_t count = 0, correct = 0, ignore = 0, almost_correct = 0;
+  const int topn = 10;
+  while (std::getline(in, line)) {
+    if (line[0] == ':') {
+      printf("%s\n", line.c_str());
+      continue;
+    }
 
-		std::istringstream iss(line);
-		std::string a, b, c, expected;
-		iss >> a >> b >> c >> expected;
-		lower(a); lower(b); lower(c); lower(expected);
-		
-		if (!model.has(a) || !model.has(b) || !model.has(c) || !model.has(expected)) {
-			printf("unhandled: %s %s %s %s\n", a.c_str(), b.c_str(), c.c_str(), expected.c_str());
-			++ignore;
-			continue;
-		}
+    std::istringstream iss(line);
+    std::string a, b, c, expected;
+    iss >> a >> b >> c >> expected;
+    lower(a); lower(b); lower(c); lower(expected);
 
-		++count;
-		std::vector<std::string> positive{b, c}, negative{a};
-		auto predict = model.most_similar(positive, negative, topn);
-		if (predict[0].first == expected) { ++ correct; ++almost_correct; }
-		else {
-			bool found = false;
-			for (auto& v: predict) { 
-				if (v.first == expected) { found = true; break; }
-			}
-			if (found) ++almost_correct;
-			else printf("predicted: %s, expected: %s\n", predict[0].first.c_str(), expected.c_str());
-		}
-	}
+    if (!model.has(a) || !model.has(b) || !model.has(c) || !model.has(expected)) {
+      printf("unhandled: %s %s %s %s\n", a.c_str(), b.c_str(), c.c_str(), expected.c_str());
+      ++ignore;
+      continue;
+    }
 
-	if (count > 0) printf("predict %lu out of %lu (%f%%), almost correct %lu (%f%%) ignore %lu\n", correct, count, correct * 100.0 / count, almost_correct, almost_correct * 100.0 / count, ignore);
+    ++count;
+    std::vector<std::string> positive{b, c}, negative{a};
+    auto predict = model.most_similar(positive, negative, topn);
+    if (predict[0].first == expected) { ++ correct; ++almost_correct; }
+    else {
+      bool found = false;
+      for (auto& v: predict) {
+	if (v.first == expected) { found = true; break; }
+      }
+      if (found) ++almost_correct;
+      else printf("predicted: %s, expected: %s\n", predict[0].first.c_str(), expected.c_str());
+    }
+  }
 
-	return 0;	
+  if (count > 0) printf("predict %lu out of %lu (%f%%), almost correct %lu (%f%%) ignore %lu\n", correct, count, correct * 100.0 / count, almost_correct, almost_correct * 100.0 / count, ignore);
+
+  return 0;
 }
 
 int main(int argc, const char *argv[])
 {
-	Word2Vec<std::string> model(200);
-	using Sentence = Word2Vec<std::string>::Sentence;
-	using SentenceP = Word2Vec<std::string>::SentenceP;
+  Word2Vec<std::string> model(200);
+  using Sentence = Word2Vec<std::string>::Sentence;
+  using SentenceP = Word2Vec<std::string>::SentenceP;
 
-	model.sample_ = 0;
-//	model.window_ = 10;
-//	model.phrase_ = true;
-	int n_workers = 4;
+  model.sample_ = 0;
+  //	model.window_ = 10;
+  //	model.phrase_ = true;
+  // int n_workers = 4;
 
-	::srand(::time(NULL));
+  ::srand(::time(NULL));
 
-	auto distance = [&model]() {
-		while (1) {
-			std::string s;
-			std::cout << "\nFind nearest word for (:quit to break):";
-			std::cin >> s;
-			if (s == ":quit") break;
-			auto p = model.most_similar(std::vector<std::string>{s}, std::vector<std::string>(), 10);
-			size_t i = 0;
-			for (auto& v: p) {
-				std::cout << i++ << " " << v.first << " " << v.second << std::endl;
-			}
-		}
-	};
+  auto distance = [&model]() {
+    while (1) {
+      std::string s;
+      std::cout << "\nFind nearest word for (:quit to break):";
+      std::cin >> s;
+      if (s == ":quit") break;
+      auto p = model.most_similar(std::vector<std::string>{s}, std::vector<std::string>(), 10);
+      size_t i = 0;
+      for (auto& v: p) {
+	std::cout << i++ << " " << v.first << " " << v.second << std::endl;
+      }
+    }
+  };
 
-	bool train = true, test = false;
-	if (argc > 1 && std::string(argv[1]) == "test") {
-		std::swap(train, test);
-	}
-	
-	if (train) {
-		std::vector<SentenceP> sentences;
+  bool train = true, test = false;
+  if (argc > 1 && std::string(argv[1]) == "test") {
+    std::swap(train, test);
+  }
 
-		size_t count =0;
-		const size_t max_sentence_len = 200;
+  if (train) {
+    std::vector<SentenceP> sentences;
 
-		SentenceP sentence(new Sentence);
-		// wget http://mattmahoney.net/dc/text8.zip
-		std::ifstream in("text8");
-		while (true) {
-			std::string s;
-			in >> s;
-			if (s.empty()) break;	
-			
-			++count;
-			sentence->tokens_.push_back(std::move(s));
-			if (count == max_sentence_len) {
-				count = 0;
-				sentence->words_.reserve(sentence->tokens_.size());
-				sentences.push_back(std::move(sentence));
-				sentence.reset(new Sentence);
-			}
-		}
-		
-		if (!sentence->tokens_.empty())
-			sentences.push_back(std::move(sentence));
+    size_t count =0;
+    const size_t max_sentence_len = 200;
 
-		auto cstart = std::chrono::high_resolution_clock::now();
-		model.build_vocab(sentences);
-		auto cend = std::chrono::high_resolution_clock::now();
-		printf("load vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+    SentenceP sentence(new Sentence);
+    // wget http://mattmahoney.net/dc/text8.zip
+    std::ifstream in("text8");
+    while (true) {
+      std::string s;
+      in >> s;
+      if (s.empty()) break;
 
-		cstart = cend;
-		model.train(sentences, n_workers);
-		cend = std::chrono::high_resolution_clock::now();
-		printf("train: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+      ++count;
+      sentence->tokens_.push_back(std::move(s));
+      if (count == max_sentence_len) {
+	count = 0;
+	sentence->words_.reserve(sentence->tokens_.size());
+	sentences.push_back(std::move(sentence));
+	sentence.reset(new Sentence);
+      }
+    }
 
-		cstart = cend;
-		model.save("vectors.bin");
-		model.save_text("vectors.txt");
-		cend = std::chrono::high_resolution_clock::now();
-		printf("save model: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+    if (!sentence->tokens_.empty())
+      sentences.push_back(std::move(sentence));
 
-//		distance();
-	}
+    auto cstart = std::chrono::high_resolution_clock::now();
+    model.build_vocab(sentences);
+    auto cend = std::chrono::high_resolution_clock::now();
+    printf("load vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
 
-	if (test) {
-		auto cstart = std::chrono::high_resolution_clock::now();
-		model.load("vectors.bin");
-		auto cend = std::chrono::high_resolution_clock::now();
-		printf("load model: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+    cstart = cend;
+    model.train(sentences);
+    cend = std::chrono::high_resolution_clock::now();
+    printf("train: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
 
-		distance();
+    cstart = cend;
+    model.save("vectors.bin");
+    model.save_text("vectors.txt");
+    cend = std::chrono::high_resolution_clock::now();
+    printf("save model: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
 
-		cstart = cend;
-		accuracy(model, "questions-words.txt");
-		cend = std::chrono::high_resolution_clock::now();
-		printf("test model: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
-	}
+    //		distance();
+  }
 
-	return 0;
+  if (test) {
+    auto cstart = std::chrono::high_resolution_clock::now();
+    model.load("vectors.bin");
+    auto cend = std::chrono::high_resolution_clock::now();
+    printf("load model: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+
+    distance();
+
+    cstart = cend;
+    accuracy(model, "questions-words.txt");
+    cend = std::chrono::high_resolution_clock::now();
+    printf("test model: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+  }
+
+  return 0;
 }
 
