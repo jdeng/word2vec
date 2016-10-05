bfca352ae37b938eb2ec3fa1bd7a6f5a820a096c
Wladimir Sidorenko <wlsidorenko@gmail.com>
2016-10-05 22:42:50 +0200

    code made compilable

--- src/word2vec.c
+++ src/word2vec.c
@@ -12,11 +12,11 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+#include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <pthread.h>
 
 #define MAX_STRING 100
 #define EXP_TABLE_SIZE 1000
@@ -34,7 +34,11 @@ struct vocab_word {
   char *word, *code, codelen;
 };
 
-char train_file[MAX_STRING], output_file[MAX_STRING];
+typedef struct {
+  unsigned long long m_id;
+  const char *m_train_fname;
+} pthread_opts_t;
+
 char save_vocab_file[MAX_STRING], read_vocab_file[MAX_STRING];
 struct vocab_word *vocab;
 int binary = 0, cbow = 1, debug_mode = 2, window = 5, min_count = 5, num_threads = 12, min_reduce = 1;
@@ -65,6 +69,57 @@ void InitUnigramTable() {
     }
     if (i >= vocab_size) i = vocab_size - 1;
   }
+}
+
+/**
+ * Print usage message and exit
+ *
+ * @param a_ret - exit code for the program
+ *
+ * @return \c void
+ */
+static void usage(int a_ret) {
+  printf("WORD VECTOR estimation toolkit v 0.1c\n\n");
+  printf("Options:\n");
+  printf("Parameters for training:\n");
+  printf("\t-train <file>\n");
+  printf("\t\tUse text data from <file> to train the model\n");
+  printf("\t-output <file>\n");
+  printf("\t\tUse <file> to save the resulting word vectors / word clusters\n");
+  printf("\t-size <int>\n");
+  printf("\t\tSet size of word vectors; default is 100\n");
+  printf("\t-window <int>\n");
+  printf("\t\tSet max skip length between words; default is 5\n");
+  printf("\t-sample <float>\n");
+  printf("\t\tSet threshold for occurrence of words. Those that appear with higher frequency in the training data\n");
+  printf("\t\twill be randomly down-sampled; default is 1e-3, useful range is (0, 1e-5)\n");
+  printf("\t-hs <int>\n");
+  printf("\t\tUse Hierarchical Softmax; default is 0 (not used)\n");
+  printf("\t-negative <int>\n");
+  printf("\t\tNumber of negative examples; default is 5, common values are 3 - 10 (0 = not used)\n");
+  printf("\t-threads <int>\n");
+  printf("\t\tUse <int> threads (default 12)\n");
+  printf("\t-iter <int>\n");
+  printf("\t\tRun more training iterations (default 5)\n");
+  printf("\t-min-count <int>\n");
+  printf("\t\tThis will discard words that appear less than <int> times; default is 5\n");
+  printf("\t-alpha <float>\n");
+  printf("\t\tSet the starting learning rate; default is 0.025 for skip-gram and 0.05 for CBOW\n");
+  printf("\t-classes <int>\n");
+  printf("\t\tOutput word classes rather than word vectors; default number of classes is 0 (vectors are written)\n");
+  printf("\t-debug <int>\n");
+  printf("\t\tSet the debug mode (default = 2 = more info during training)\n");
+  printf("\t-binary <int>\n");
+  printf("\t\tSave the resulting vectors in binary moded; default is 0 (off)\n");
+  printf("\t-save-vocab <file>\n");
+  printf("\t\tThe vocabulary will be saved to <file>\n");
+  printf("\t-read-vocab <file>\n");
+  printf("\t\tThe vocabulary will be read from <file>, not constructed from the training data\n");
+  printf("\t-cbow <int>\n");
+  printf("\t\tUse the continuous bag of words model; default is 1 (use 0 for skip-gram model)\n");
+  printf("\nExamples:\n");
+  printf("./word2vec -train data.txt -output vec.txt -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -binary 0 -cbow 1 -iter 3\n\n");
+  exit(a_ret);
 }
 
 // Reads a single word from a file, assuming space + tab + EOL to be word boundaries
@@ -259,7 +314,7 @@ void CreateBinaryTree() {
   free(parent_node);
 }
 
-void LearnVocabFromTrainFile() {
+void LearnVocabFromTrainFile(const char train_file[]) {
   char word[MAX_STRING];
   FILE *fin;
   long long a, i;
@@ -302,7 +357,7 @@ void SaveVocab() {
   fclose(fo);
 }
 
-void ReadVocab() {
+void ReadVocab(const char train_file[]) {
   long long a, i = 0;
   char c;
   char word[MAX_STRING];
@@ -317,7 +372,10 @@ void ReadVocab() {
     ReadWord(word, fin);
     if (feof(fin)) break;
     a = AddWordToVocab(word);
-    fscanf(fin, "%lld%c", &vocab[a].cn, &c);
+    if (fscanf(fin, "%lld%c", &vocab[a].cn, &c) != 2) {
+      printf("Invalid line format.\n");
+      exit(2);
+    }
     i++;
   }
   SortVocab();
@@ -359,17 +417,17 @@ void InitNet() {
   CreateBinaryTree();
 }
 
-void *TrainModelThread(void *id) {
+void *TrainModelThread(void *opts) {
   long long a, b, d, cw, word, last_word, sentence_length = 0, sentence_position = 0;
   long long word_count = 0, last_word_count = 0, sen[MAX_SENTENCE_LENGTH + 1];
   long long l1, l2, c, target, label, local_iter = iter;
-  unsigned long long next_random = (long long)id;
+  unsigned long long next_random = ((pthread_opts_t *) opts)->m_id;
   real f, g;
   clock_t now;
   real *neu1 = (real *)calloc(layer1_size, sizeof(real));
   real *neu1e = (real *)calloc(layer1_size, sizeof(real));
-  FILE *fi = fopen(train_file, "rb");
-  fseek(fi, file_size / (long long)num_threads * (long long)id, SEEK_SET);
+  FILE *fi = fopen(((pthread_opts_t *) opts)->m_train_fname, "rb");
+  fseek(fi, file_size / (long long)num_threads * (long long)next_random, SEEK_SET);
   while (1) {
     if (word_count - last_word_count > 10000) {
       word_count_actual += word_count - last_word_count;
@@ -410,7 +468,7 @@ void *TrainModelThread(void *id) {
       word_count = 0;
       last_word_count = 0;
       sentence_length = 0;
-      fseek(fi, file_size / (long long)num_threads * (long long)id, SEEK_SET);
+      fseek(fi, file_size / (long long)num_threads * (long long)next_random, SEEK_SET);
       continue;
     }
     word = sen[sentence_position];
@@ -541,19 +599,30 @@ void *TrainModelThread(void *id) {
   pthread_exit(NULL);
 }
 
-void TrainModel() {
+void train_model(const char train_file[], const char output_file[]) {
   long a, b, c, d;
   FILE *fo;
   pthread_t *pt = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
   printf("Starting training using file %s\n", train_file);
   starting_alpha = alpha;
-  if (read_vocab_file[0] != 0) ReadVocab(); else LearnVocabFromTrainFile();
+
+  if (read_vocab_file[0] != 0)
+    ReadVocab(train_file);
+  else
+    LearnVocabFromTrainFile(train_file);
+
   if (save_vocab_file[0] != 0) SaveVocab();
   if (output_file[0] == 0) return;
   InitNet();
   if (negative > 0) InitUnigramTable();
   start = clock();
-  for (a = 0; a < num_threads; a++) pthread_create(&pt[a], NULL, TrainModelThread, (void *)a);
+
+  pthread_opts_t opts;
+  opts.m_train_fname = (const char *) train_file;
+  for (a = 0; a < num_threads; ++a) {
+    opts.m_id = a;
+    pthread_create(&pt[a], NULL, TrainModelThread, (void *) &opts);
+  }
   for (a = 0; a < num_threads; a++) pthread_join(pt[a], NULL);
   fo = fopen(output_file, "wb");
   if (classes == 0) {
@@ -625,53 +694,20 @@ int ArgPos(char *str, int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-  int i;
-  if (argc == 1) {
-    printf("WORD VECTOR estimation toolkit v 0.1c\n\n");
-    printf("Options:\n");
-    printf("Parameters for training:\n");
-    printf("\t-train <file>\n");
-    printf("\t\tUse text data from <file> to train the model\n");
-    printf("\t-output <file>\n");
-    printf("\t\tUse <file> to save the resulting word vectors / word clusters\n");
-    printf("\t-size <int>\n");
-    printf("\t\tSet size of word vectors; default is 100\n");
-    printf("\t-window <int>\n");
-    printf("\t\tSet max skip length between words; default is 5\n");
-    printf("\t-sample <float>\n");
-    printf("\t\tSet threshold for occurrence of words. Those that appear with higher frequency in the training data\n");
-    printf("\t\twill be randomly down-sampled; default is 1e-3, useful range is (0, 1e-5)\n");
-    printf("\t-hs <int>\n");
-    printf("\t\tUse Hierarchical Softmax; default is 0 (not used)\n");
-    printf("\t-negative <int>\n");
-    printf("\t\tNumber of negative examples; default is 5, common values are 3 - 10 (0 = not used)\n");
-    printf("\t-threads <int>\n");
-    printf("\t\tUse <int> threads (default 12)\n");
-    printf("\t-iter <int>\n");
-    printf("\t\tRun more training iterations (default 5)\n");
-    printf("\t-min-count <int>\n");
-    printf("\t\tThis will discard words that appear less than <int> times; default is 5\n");
-    printf("\t-alpha <float>\n");
-    printf("\t\tSet the starting learning rate; default is 0.025 for skip-gram and 0.05 for CBOW\n");
-    printf("\t-classes <int>\n");
-    printf("\t\tOutput word classes rather than word vectors; default number of classes is 0 (vectors are written)\n");
-    printf("\t-debug <int>\n");
-    printf("\t\tSet the debug mode (default = 2 = more info during training)\n");
-    printf("\t-binary <int>\n");
-    printf("\t\tSave the resulting vectors in binary moded; default is 0 (off)\n");
-    printf("\t-save-vocab <file>\n");
-    printf("\t\tThe vocabulary will be saved to <file>\n");
-    printf("\t-read-vocab <file>\n");
-    printf("\t\tThe vocabulary will be read from <file>, not constructed from the training data\n");
-    printf("\t-cbow <int>\n");
-    printf("\t\tUse the continuous bag of words model; default is 1 (use 0 for skip-gram model)\n");
-    printf("\nExamples:\n");
-    printf("./word2vec -train data.txt -output vec.txt -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -binary 0 -cbow 1 -iter 3\n\n");
-    return 0;
-  }
+  if (argc < 2)
+    usage(1);
+
+  char train_file[MAX_STRING], output_file[MAX_STRING];
   output_file[0] = 0;
   save_vocab_file[0] = 0;
   read_vocab_file[0] = 0;
+  train_file[0] = 0;
+  output_file[0] = 0;
+
+  const char *opt = NULL;
+  for (int i = 0; i < argc; ++i) {
+    
+  }
   if ((i = ArgPos((char *)"-size", argc, argv)) > 0) layer1_size = atoi(argv[i + 1]);
   if ((i = ArgPos((char *)"-train", argc, argv)) > 0) strcpy(train_file, argv[i + 1]);
   if ((i = ArgPos((char *)"-save-vocab", argc, argv)) > 0) strcpy(save_vocab_file, argv[i + 1]);
@@ -697,6 +733,6 @@ int main(int argc, char **argv) {
     expTable[i] = exp((i / (real)EXP_TABLE_SIZE * 2 - 1) * MAX_EXP); // Precompute the exp() table
     expTable[i] = expTable[i] / (expTable[i] + 1);                   // Precompute f(x) = x / (x + 1)
   }
-  TrainModel();
+  train_model(train_file, output_file);
   return 0;
 }
